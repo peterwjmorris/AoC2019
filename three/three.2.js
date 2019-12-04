149edc362b88f93e96e8680fa38d948f85bf89db
Pete Morris <peter@isfluent.com>
2019-12-04 09:01:52 +0000

    Three (2)

--- three/three.2.js
+++ three/three.2.js
@@ -4,11 +4,11 @@ import _ from "lodash";
 import { int, stringLen, newline } from "parjs";
 import { manySepBy, then } from "parjs/combinators";
 
-const addTo = (x, ab) => (x || "") + ab;
+const addTo = (x, ab, tot) => x || tot;
 
 const move = (state, dir, dist, ab) => {
     for (var x = 0; x < dist; x++) {
-
+        state.tot++;
         switch (dir) {
             case "U":
                 state.posy++;
@@ -24,9 +24,9 @@ const move = (state, dir, dist, ab) => {
                 break;
         }
 
-        state.box[([state.posx, state.posy])] = addTo(state.box[([state.posx, state.posy])], ab);
+        if (ab == "A") state.box[([state.posx, state.posy])] = addTo(state.box[([state.posx, state.posy])], ab, state.tot);
 
-        if (state.box[([state.posx, state.posy])].indexOf("AB") >= 0) state.intersections.push(Math.abs(state.posx) + Math.abs(state.posy));
+        if (ab == "B" && state.box[([state.posx, state.posy])] > 0) state.intersections.push([Math.abs(state.posx) + Math.abs(state.posy), state.box[([state.posx, state.posy])] + state.tot]);
     }
 }
 
@@ -34,13 +34,15 @@ fs.readFile('three/input', 'utf8', (err, data) => {
     const wireParser = manySepBy(",")(then(int())(stringLen(1)));
     var wires = manySepBy(newline())(wireParser).parse(data).value;
 
+
     const box = {};
     box[([0,0])] = "O";
 
     const state = {
         box: box,
         posx: 0,
-        posy: 0
+        posy: 0,
+        tot: 0
     }
 
     _.forEach(wires[0], ([dir, dist]) => move(state, dir, dist, "A"));
@@ -49,12 +51,13 @@ fs.readFile('three/input', 'utf8', (err, data) => {
         box: state.box,
         posx: 0,
         posy: 0,
-        intersections: []
+        intersections: [],
+        tot: 0
     }
 
     _.forEach(wires[1], ([dir, dist]) => move(state2, dir, dist, "B"));
 
     console.log(state2.intersections);
+    console.log(_.minBy(state2.intersections, x => x[1]));
 
-    console.log(_.min(state2.intersections));
 });
